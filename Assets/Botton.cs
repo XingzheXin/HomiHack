342987d49aa895e5655bb3067a71724173ac8d5a
xingzhexin <xingzhexin@outlook.com>
2018-04-07 11:53:14 -0500

    asdf

---
+++ Assets/Botton.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botton : MonoBehaviour {
+    public void onButtonDown() {
+        Debug.Log("Button Down");
+    }
+}
