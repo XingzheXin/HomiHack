c6ee5560838da6fec8061020fd77d9c22df0d69e
Kah Hin Lai <laixx330@umn.edu>
2018-04-08 00:07:21 -0500

    remove something

--- Assets/Botton.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Botton : MonoBehaviour {
-    public void onButtonDown() {
-        Debug.Log("Button Down");
-    }
-}
